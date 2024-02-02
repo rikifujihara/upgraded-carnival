d1082ffb6969566833090c746e9f78551a9b2ebd
rikifujihara <rikifujihara@gmail.com>
2024-02-02 19:45:06 +1100

    refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,14 @@
-import { CORE_CONCEPTS } from "./data.js";
 import Header from "./components/Header.jsx";
-import CoreConcept from "./components/CoreConcept.jsx";
-import TabButton from "./components/TabButton.jsx";
+import CoreConcepts from "./components/CoreConcepts.jsx";
+import Examples from "./components/examples.jsx";
 
 function App() {
-  let tabContent = "Please click a button";
-
-  function handleSelect(selectedButton) {
-    tabContent = selectedButton;
-  }
-
   return (
     <div>
       <Header></Header>
       <main>
-        <section id="core-concepts">
-          <h2>Core Concepts</h2>
-          <ul>
-            <CoreConcept {...CORE_CONCEPTS[0]} />
-            <CoreConcept {...CORE_CONCEPTS[1]} />
-            <CoreConcept {...CORE_CONCEPTS[2]} />
-            <CoreConcept {...CORE_CONCEPTS[3]} />
-          </ul>
-        </section>
-        <section id="examples">
-          <h2>Examples</h2>
-          <menu>
-            <TabButton onSelect={() => handleSelect("components")}>
-              Components
-            </TabButton>
-            <TabButton onSelect={() => handleSelect("jsx")}>JSX</TabButton>
-            <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
-            <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
-          </menu>
-          {tabContent}
-        </section>
+        <CoreConcepts />
+        <Examples />
       </main>
     </div>
   );
--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,7 +1,10 @@
-export default function TabButton({ children, onSelect }) {
+export default function TabButton({ children, isSelected, ...props }) {
+  console.log("Tab() executing");
   return (
     <li>
-      <button onClick={onSelect}>{children}</button>
+      <button className={isSelected ? "active" : ""} {...props}>
+        {children}
+      </button>
     </li>
   );
 }
